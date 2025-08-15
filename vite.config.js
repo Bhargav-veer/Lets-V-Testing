595c84d395f60c788d508eb52d4211c549070943
Bhargav Veeramalla <bhargavveera1@gmail.com>
2025-08-15 18:08:47 +0530

    message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Detect if running on Netlify
+const isNetlify = process.env.NETLIFY === 'true';
+
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  base: "/", // 👈 Replace with your GitHub repo name
-})
+  base: isNetlify ? '/' : '/Lets-V-Testing/', // Change this to your repo name
+});
